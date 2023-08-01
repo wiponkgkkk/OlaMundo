4e2dfe184b014f60bea8b3f896902cc17e4b993f
William José <williamdelas222@gmail.com>
2023-08-01 09:58:58 -0300

    Commit - Exercicio02

---
+++ exercicio01.py
@@ -0,0 +1,13 @@
+#Inputs
+
+x = float(input("Qual foi sua nota ? " ))
+y = float(input("Qual foi sua nota ? " ))
+z = float(input("Qual foi sua nota ? " ))
+
+#Cálculos
+
+media = (x + y + z)/3
+
+#Prints
+
+print("Sua media foi igual a: ", media)
---
+++ exercicio02.py
@@ -0,0 +1,11 @@
+#Input
+num = int(input("Diga um numero: " ))
+
+#Cálculos
+
+sucessor = num + 1
+antecessor = num - 1
+
+#Prints
+
+print("Seu sucessor é", sucessor, "e seu antecessor é", antecessor)
