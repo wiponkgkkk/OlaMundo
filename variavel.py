22a22b91227ba7a0db2c97e4a58a61c2212f7835
William José <williamdelas222@gmail.com>
2023-08-01 09:22:51 -0300

    Commit - Variavel

--- variavel.py
+++ variavel.py
@@ -1,4 +1,58 @@
-# input é uma função que permite perguntar algo ao ususario
-nome = input("Qual é o seu nome? ")
+nome = input("Qual é o seu nome?")
 
-print("Olá, ", nome)
+
+print("Olá,",nome,".Tudo bem com você?")
+
+print(type(nome))
+
+a = 10
+b = 5
+c = "Rio de Janeiro"
+d = "True"
+
+print("a: ",a)
+print("b: ",b)
+print("c: ",c)
+print("d: ",d)
+
+print("Tipo da var a: ", type(a))# tipo inteiro (numeros inteiros)
+print("Tipo da var b: ", type(b))# tipo float (numeros reais)
+print("Tipo da var c: ", type(c))# tipo string (alfanumerico)
+print("Tipo da var d: ", type(d))# tipo boolean (True or False)
+
+a = 20
+print("a: ",a)
+b = "São Paulo"
+print("b :",b)
+print("Tipo da var a: ", type(a))
+print("Tipo da var b: ", type(b))
+
+a = input("Informe um número: ")
+b = input("Informe outro número: ")
+print("a: ",a,"-b: ",b)
+print("Tipo da var a: ", type(a))
+print("Tipo da var b: ", type(b))
+c = a + b  # cocatenou as strings de a e b (juntou elas)
+print("c: ",c)
+print("Tipo da var c: ", type(c))
+d = int(a)
+print("d: ",d)
+print("Tipo da var d: ", type(d))
+
+a = input("Informe um número: ")
+b = input("Informe outro número: ")
+print("a: ",a,"-b: ",b)
+print("Tipo da var a: ", type(a))
+print("Tipo da var b: ", type(b))
+c = a + b
+print("c: ",c)
+print("Tipo da var c: ", type(c))
+
+a = float(input("Informe um número: "))
+b = float(input("Informe outro número: "))
+print("a: ",a,"-b: ",b)
+print("Tipo da var a: ", type(a))
+print("Tipo da var b: ", type(b))
+c = a + b
+print("c: ",c)
+print("Tipo da var c: ", type(c))
